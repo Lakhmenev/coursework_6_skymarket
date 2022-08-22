27613a570ae1f40a18b06a7c5133285b77e0eb7e
Dmitriy Lahmenev <lahmenev@gmail.com>
2022-08-22 09:40:48 +0300

    edit urls

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -6,7 +6,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/admin/", admin.site.urls),
     path("api/redoc-tasks/", include("redoc.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
